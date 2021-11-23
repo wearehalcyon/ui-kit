f07afbf30a03cbbb81fb36ab146c43d8efa410a3
Roman <roman@Romans-iMac.local>
2021-11-23 15:43:53 +0200

    Initial commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 'use strict';
 
 jQuery(document).ready(function($){
-    
+    $(".nav-item a").click(function(e) {
+        $(".nav-item a").removeClass('button-primary');
+        $(this).addClass('button-primary');
+        e.preventDefault();
+        var aid = $(this).attr("href");
+        $('html,body').animate({scrollTop: $(aid).offset().top - 80 + 'px'},'slow');
+    });
 });
