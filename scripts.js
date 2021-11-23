7ea88c8bac3dcd3a09d92498c2b1d8c4172d4d68
Roman <roman@Romans-iMac.local>
2021-11-23 16:38:30 +0200

    Initial commit

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@
 
 jQuery(document).ready(function($){
     $(".nav-item a").click(function(e) {
+        let title = $(this).text();
+        if (title == 'Back to Top') {
+            $('.head-title h2.title').text('Documentation');
+        } else {
+            $('.head-title h2.title').text(title);
+        }
+        
         $(".nav-item a").removeClass('button-primary');
         $(this).addClass('button-primary');
         e.preventDefault();
